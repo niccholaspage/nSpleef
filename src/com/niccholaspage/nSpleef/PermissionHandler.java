6e866e8ba3a0c930ed2dcef8936d6c3b32e16221
niccholaspage <niccholaspage@gmail.com>
2011-03-30 23:13:42 -0400

    Forgot file

---
+++ src/com/niccholaspage/nSpleef/PermissionHandler.java
@@ -0,0 +1,41 @@
+package com.niccholaspage.nSpleef;
+
+import org.anjocaido.groupmanager.GroupManager;
+import org.bukkit.Server;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+import com.nijikokun.bukkit.Permissions.Permissions;
+
+public class PermissionHandler {
+	private enum PermissionType {
+		PERMISSIONS,
+		GROUP_MANAGER
+	}
+	private static PermissionType type;
+	private static Plugin permissionPlugin;
+	public static void init(Server server){
+		Plugin groupManager = server.getPluginManager().getPlugin("GroupManager");
+		Plugin permissions = server.getPluginManager().getPlugin("Permissions");
+		
+		if (groupManager != null){
+			permissionPlugin = groupManager;
+			type = PermissionType.GROUP_MANAGER;
+			System.out.println("[nSpleef] Hooked into Group Manager " + groupManager.getDescription().getVersion());
+		}else if (permissions != null){
+			permissionPlugin = permissions;
+			type = PermissionType.PERMISSIONS;
+			System.out.println("[nSpleef] Hooked into Permissions " + permissions.getDescription().getVersion());
+		}else {
+			System.out.println("[nSpleef] Could not find any Permissions plugin, disabling.");
+			server.getPluginManager().disablePlugin(server.getPluginManager().getPlugin("nSpleef"));
+		}
+	}
+	public static boolean has(Player player, String node){
+		switch (type){
+		case PERMISSIONS: return ((Permissions)permissionPlugin).getHandler().has(player, node);
+		case GROUP_MANAGER: return ((GroupManager)permissionPlugin).getWorldsHolder().getWorldPermissions(player).has(player, node);
+		default: return false;
+		}
+	}
+}
