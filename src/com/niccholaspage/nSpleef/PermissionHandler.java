1a994618ba3c47a73ce70be51ee566b43361f52f
niccholaspage <niccholaspage@gmail.com>
2011-03-31 06:58:43 -0400

    No permissions support

--- src/com/niccholaspage/nSpleef/PermissionHandler.java
+++ src/com/niccholaspage/nSpleef/PermissionHandler.java
@@ -10,7 +10,8 @@ import com.nijikokun.bukkit.Permissions.Permissions;
 public class PermissionHandler {
 	private enum PermissionType {
 		PERMISSIONS,
-		GROUP_MANAGER
+		GROUP_MANAGER,
+		NONE
 	}
 	private static PermissionType type;
 	private static Plugin permissionPlugin;
@@ -27,14 +28,14 @@ public class PermissionHandler {
 			type = PermissionType.PERMISSIONS;
 			System.out.println("[nSpleef] Hooked into Permissions " + permissions.getDescription().getVersion());
 		}else {
-			System.out.println("[nSpleef] Could not find any Permissions plugin, disabling.");
-			server.getPluginManager().disablePlugin(server.getPluginManager().getPlugin("nSpleef"));
+			System.out.println("[nSpleef] Could not find any permissions plugin. Only OP will be able to use admin commands.");
 		}
 	}
 	public static boolean has(Player player, String node){
 		switch (type){
 		case PERMISSIONS: return ((Permissions)permissionPlugin).getHandler().has(player, node);
 		case GROUP_MANAGER: return ((GroupManager)permissionPlugin).getWorldsHolder().getWorldPermissions(player).has(player, node);
+		case NONE: if (node.startsWith("nSpleef.admin")) return player.isOp(); else return true;
 		default: return false;
 		}
 	}
