5efc93e0a3bd79c259fb639c1ab18bdc938f92a7
niccholaspage <niccholaspage@gmail.com>
2011-04-05 18:39:54 -0400

    Updated creategame.

--- src/com/niccholaspage/nSpleef/commands/CreateGameCommand.java
+++ src/com/niccholaspage/nSpleef/commands/CreateGameCommand.java
@@ -8,6 +8,7 @@ import org.bukkit.entity.Player;
 
 import com.niccholaspage.nSpleef.EconomyHandler;
 import com.niccholaspage.nSpleef.EconomyHandler.EconomyType;
+import com.niccholaspage.nSpleef.Filter;
 import com.niccholaspage.nSpleef.PermissionHandler;
 import com.niccholaspage.nSpleef.Util;
 import com.niccholaspage.nSpleef.Volume;
@@ -35,25 +36,16 @@ public class CreateGameCommand implements CommandExecutor {
 		 }
 		 }
 		 String name = args[1] + "," + args[2].toLowerCase() + "," + player.getName();
-		 Boolean passed = false;
 		 Integer where = 0;
-		 for (int i = 0; i<= plugin.nSpleefArenas.size() - 1; i++){
-		 if (args[2].equalsIgnoreCase(plugin.nSpleefArenas.get(i).getName())){
-			 passed = true;
-			 where = i;
-		 }
-		 }
-		 if (passed == false){
+		 if (Filter.getArenaByName(args[2]) == null){
 			 player.sendMessage(ChatColor.DARK_PURPLE + "The arena does not exist.");
-			 return true;
+			 return true; 
 		 }
-		 if (!(plugin.nSpleefGames.size() == 0)){
-		 for (int i = 0; i<= plugin.nSpleefGames.size() - 1; i++){
-			 if (args[1].equalsIgnoreCase((plugin.nSpleefGames.get(i).split(",")[0]))){
+		 if (!(Filter.getGameByName(args[1]) == null)){
+			 if (args[1].equalsIgnoreCase((Filter.getGameByName(args[1]).split(",")[0]))){
 				 player.sendMessage(ChatColor.DARK_PURPLE + "A game with that name already exists.");
 				 return true;
 			 }
-		 }
 		 }
 		 if (args[1].contains(",")){
 			 player.sendMessage(ChatColor.DARK_PURPLE + "A game name cannot contain a comma.");
