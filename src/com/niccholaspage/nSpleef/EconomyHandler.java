c99146298a22d93927f3173cefef99bb6b7a6f70
niccholaspage <niccholaspage@gmail.com>
2011-04-13 21:41:12 -0400

    Fixed MineConomy integration

--- src/com/niccholaspage/nSpleef/EconomyHandler.java
+++ src/com/niccholaspage/nSpleef/EconomyHandler.java
@@ -18,11 +18,12 @@ public class EconomyHandler {
 	}
 	public static EconomyType type;
 	private static BOSEconomy bosecon;
-	private static MineConomy mineConomy;
+	private static MineConomy mineCon;
 	
 	public static void init(Server server){
 		Plugin iconomy = server.getPluginManager().getPlugin("iConomy");
 		Plugin boseconomy = server.getPluginManager().getPlugin("BOSEconomy");
+		Plugin mineConomy = server.getPluginManager().getPlugin("MineConomy");
 		if (iconomy != null){
 			type = EconomyType.ICONOMY;
 			System.out.println("[nSpleef] Hooked into iConomy " + iconomy.getDescription().getVersion());
@@ -32,7 +33,7 @@ public class EconomyHandler {
 			System.out.println("[nSpleef] Hooked into BOSEconomy " + boseconomy.getDescription().getVersion());
 		}else if (mineConomy != null){
 			type = EconomyType.MINECONOMY;
-			mineConomy = (MineConomy)mineConomy;
+			mineCon = (MineConomy)mineConomy;
 			System.out.println("[nSpleef] Hooked into MineConomy " + mineConomy.getDescription().getVersion());
 		}else {
 			type = EconomyType.NONE;
@@ -43,21 +44,21 @@ public class EconomyHandler {
 		switch (type){
 			case ICONOMY: iConomy.getBank().getAccount(player.getName()).add(amount); break;
 			case BOSECONOMY: bosecon.addPlayerMoney(player.getName(), amount, false); break;
-			case MINECONOMY: mineConomy.getBank().add(player.getName(), amount); break;
+			case MINECONOMY: mineCon.getBank().add(player.getName(), amount); break;
 		}
 		}
 	public static void removeMoney(Player player, Integer amount){
 		switch (type){
 		case ICONOMY: iConomy.getBank().getAccount(player.getName()).subtract(amount); break;
 		case BOSECONOMY: bosecon.setPlayerMoney(player.getName(), bosecon.getPlayerMoney(player.getName()) - amount, false); break;
-		case MINECONOMY: mineConomy.getBank().subtract(player.getName(), amount);
+		case MINECONOMY: mineCon.getBank().subtract(player.getName(), amount);
 		}
 	}
 	public static Integer getMoney(Player player){
 		switch (type){
 		case ICONOMY: return (int)iConomy.getBank().getAccount(player.getName()).getBalance();
 		case BOSECONOMY: return bosecon.getPlayerMoney(player.getName());
-		case MINECONOMY: return (int)mineConomy.getBank().getTotal(player.getName());
+		case MINECONOMY: return (int)mineCon.getBank().getTotal(player.getName());
 		default: return null;
 		}
 	}
