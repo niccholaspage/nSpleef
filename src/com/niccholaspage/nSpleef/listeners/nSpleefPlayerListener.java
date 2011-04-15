27d6a7284dc34bda0cbf656b12c249e3decb277a
niccholaspage <niccholaspage@gmail.com>
2011-04-15 16:27:30 -0400

    Fixed a bug & took out the backup of the old onPlayerMove

--- src/com/niccholaspage/nSpleef/listeners/nSpleefPlayerListener.java
+++ src/com/niccholaspage/nSpleef/listeners/nSpleefPlayerListener.java
@@ -41,68 +41,15 @@ public class nSpleefPlayerListener extends PlayerListener{
 		  if (loc.getBlockY() + 1 <= theblock.getBlockY() + 1){
 			  for (int i = 0; i < arena.getPlayersIn().size(); i++){
 				  arena.getPlayersIn().get(i).sendMessage(ChatColor.DARK_PURPLE + "[nSpleef] " + player.getDisplayName() + " is out!");
-				  arena.getPlayers().remove(player);
-				  if (arena.getPlayers().size() == 1){
-					  event.setFrom(arena.getPlayersLocation().get(arena.getPlayersIn().indexOf(player)));
-					  event.setTo(arena.getPlayersLocation().get(arena.getPlayersIn().indexOf(player)));
-				  }
-					 arena.checkLeave();
 			  }
+			  if (arena.getPlayers().size() == 1){
+				  event.setFrom(arena.getPlayersLocation().get(arena.getPlayersIn().indexOf(player)));
+				  event.setTo(arena.getPlayersLocation().get(arena.getPlayersIn().indexOf(player)));
+			  }
+			  arena.getPlayers().remove(player);
+			  arena.checkLeave();
 		  }
 	  }
-		 public void onPlayerMove2(PlayerMoveEvent event){
-			 Player player = event.getPlayer();
-			 Location loc = player.getLocation();
-			 Boolean pass = false;
-			 if (plugin.nSpleefArenas.size() == 0) return;
-			 for (int i = 0; i <= plugin.nSpleefArenas.size() - 1; i++){
-				 if (plugin.nSpleefArenas.get(i).getPlayers().contains(player)){
-					 pass = true;
-				 }
-			 }
-			 if (pass == false){
-				 return;
-			 }
-			 BlockVector theblock = new BlockVector();
-				    for (int i = 0; i <= plugin.nSpleefArenas.size() - 1; i++) {
-						 if ((Util.returnBlockInArea(Util.toVector(loc.getBlock()), plugin.nSpleefArenas.get(i).getFirstBlock(), plugin.nSpleefArenas.get(i).getSecondBlock())) == true) {
-								 if (player.getWorld().toString().equals(plugin.nSpleefArenas.get(i).getWorld().toString())){
-									 Boolean con = false;
-									 for (int j = 0; j<= plugin.nSpleefGames.size() - 1; j++){
-										 if (plugin.nSpleefGames.get(j).getArena().equalsIgnoreCase(plugin.nSpleefArenas.get(i).getName())){
-											 con = true;
-											 break;
-										 }
-									 }
-									 if (con == false){
-										 return;
-									 }
-									 if (plugin.nSpleefArenas.get(i).getFirstBlock().getBlockY() > plugin.nSpleefArenas.get(i).getSecondBlock().getBlockY()){
-										 theblock = plugin.nSpleefArenas.get(i).getSecondBlock();
-									 }
-									 if (plugin.nSpleefArenas.get(i).getFirstBlock().getBlockY() < plugin.nSpleefArenas.get(i).getSecondBlock().getBlockY()){
-										 theblock = plugin.nSpleefArenas.get(i).getFirstBlock();
-									 } 
-										 if (loc.getBlockY() + 1 <= theblock.getBlockY() + 1){
-											 for (int k = 0; k <= plugin.nSpleefArenas.size() - 1; k++){
-												 if (plugin.nSpleefArenas.get(k).getPlayers().contains(player)){
-													 for (int j = 0; j < plugin.nSpleefArenas.get(k).getPlayersIn().size(); j++){
-														 plugin.nSpleefArenas.get(k).getPlayersIn().get(j).sendMessage(ChatColor.DARK_PURPLE + "[nSpleef] " + player.getDisplayName() + " is out!");
-													 }
-													 plugin.nSpleefArenas.get(k).getPlayers().remove(player);
-													 if (plugin.nSpleefArenas.get(k).getPlayers().size() == 1){
-													 event.setFrom(plugin.nSpleefArenas.get(k).getPlayersLocation().get(plugin.nSpleefArenas.get(k).getPlayersIn().indexOf(player)));
-													 event.setTo(plugin.nSpleefArenas.get(k).getPlayersLocation().get(plugin.nSpleefArenas.get(k).getPlayersIn().indexOf(player)));
-													 }
-													 plugin.nSpleefArenas.get(k).checkLeave();
-													 return;
-												 }
-											 }
-										 }
-								 }
-				    	}
-				    }
-		 }
 	  @Override
 		 public void onPlayerInteract(PlayerInteractEvent event){
 			 if (!(event.getAction().equals(Action.RIGHT_CLICK_BLOCK))) return;
