2e4f52d5c46b3bce2b9fc9e7024b23022824ade2
niccholaspage <niccholaspage@gmail.com>
2011-04-15 16:00:58 -0400

    Fixed a bug in entity listener and updated onEntityDamage

--- src/com/niccholaspage/nSpleef/listeners/nSpleefEntityListener.java
+++ src/com/niccholaspage/nSpleef/listeners/nSpleefEntityListener.java
@@ -5,6 +5,7 @@ import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityListener;
 
+import com.niccholaspage.nSpleef.Filter;
 import com.niccholaspage.nSpleef.Util;
 import com.niccholaspage.nSpleef.nSpleef;
 
@@ -18,18 +19,16 @@ public class nSpleefEntityListener extends EntityListener {
 		 if (plugin.nSpleefArenas.size() == 0) return;
 		 for (int i = 0; i < plugin.nSpleefArenas.size(); i++){
 			 if (Util.returnBlockInArea(Util.toVector(event.getLocation().getBlock()), plugin.nSpleefArenas.get(i).getFirstBlock(), plugin.nSpleefArenas.get(i).getSecondBlock())){
+				 if (plugin.nSpleefArenas.get(i).getWorld() == event.getLocation().getWorld()){
 				 event.setCancelled(true);
+				 }
 			 }
 		 }
 	 }
 	 @Override
 	 public void onEntityDamage(EntityDamageEvent event){
 		 if (!(event.getEntity() instanceof Player)) return;
-		 Player player = (Player)event.getEntity();
-		 for (int i = 0; i < plugin.nSpleefArenas.size(); i++){
-			 if (plugin.nSpleefArenas.get(i).getPlayersIn().contains(player)){
-				 event.setCancelled(true);
-			 }
-		 }
+		 if (Filter.getArenaByPlayer((Player)event.getEntity()) == null) return;
+		 event.setCancelled(true);
 	 }
 }
