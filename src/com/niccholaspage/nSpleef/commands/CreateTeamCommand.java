b3b35582cf67f897f8637fe8ee8b9e353e80c99f
niccholaspage <niccholaspage@gmail.com>
2011-04-16 11:12:16 -0400

    Is there anyway to STOP forgetting to commit new files?

---
+++ src/com/niccholaspage/nSpleef/commands/CreateTeamCommand.java
@@ -0,0 +1,41 @@
+package com.niccholaspage.nSpleef.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.DyeColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.niccholaspage.nSpleef.Filter;
+import com.niccholaspage.nSpleef.Util;
+
+public class CreateTeamCommand implements CommandExecutor {
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+		Player player = (Player) sender;
+		 if (args.length < 3) return false;
+		 if (Util.exists("arenas.txt") == false) {
+			 player.sendMessage(ChatColor.DARK_PURPLE + "No arenas!");						 
+			 return true;
+		 }
+		 if (Filter.getArenaByName(args[1]) == null){
+			 player.sendMessage(ChatColor.DARK_PURPLE + "That arena does not exist.");
+			 return true;
+		 }
+		 boolean con = false;
+		 for (int i = 0; i < DyeColor.values().length; i++){
+			 if (DyeColor.values()[i].name().equalsIgnoreCase(args[2])){
+				 con = true;
+			 }
+		 }
+		 if (con == false){
+			 player.sendMessage(ChatColor.DARK_PURPLE + "That is not a color! Remember, you can only use wool colors.");
+			 return true;
+		 }
+		 Util.openfile("teams.txt");
+		 Util.writefile(Filter.getArenaByName(args[1]).getName() + "," + args[2].toLowerCase() + "\n");
+		 Util.closefile();
+		 player.sendMessage(ChatColor.DARK_PURPLE + "Team " + args[2].toLowerCase() + " has been made.");
+		return true;
+	}
+}
