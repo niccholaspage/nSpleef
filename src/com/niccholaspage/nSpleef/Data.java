5253db22b1bff1d008801499c5e50cac250a3a07
Nicholas niccholaspage Nassar <niccholaspage@gmail.com>
2011-05-05 18:21:20 -0400

    Test to fix MultiVerse loading up after nSpleef, causing nSpleef to fail at multiworld arenas

--- src/com/niccholaspage/nSpleef/Data.java
+++ src/com/niccholaspage/nSpleef/Data.java
@@ -16,15 +16,33 @@ public class Data {
 		    data = Util.filetoarray();
 		    Util.closefileread();
 		    for (int i = 0; i < data.size(); i++) {
-		    	nSpleefArena a = new nSpleefArena(data.get(i).split(":")[0],plugin.getServer().getWorld(data.get(i).split(":")[7]), plugin);
-		    	a.setFirstBlock(Util.toVectorxyz(Integer.parseInt((data.get(i).split(":")[1])), Integer.parseInt((data.get(i).split(":")[2])), Integer.parseInt((data.get(i).split(":")[3]))));
-		    	a.setSecondBlock(Util.toVectorxyz(Integer.parseInt((data.get(i).split(":")[4])), Integer.parseInt((data.get(i).split(":")[5])), Integer.parseInt((data.get(i).split(":")[6]))));
-		    	a.setTpBlock();
-				a.createVolume();
-				a.getVolume().setCornerOne(a.getFirstBlock().toLocation(a.getWorld()).getBlock());
-				a.getVolume().setCornerTwo(a.getSecondBlock().toLocation(a.getWorld()).getBlock());
-				a.getVolume().saveBlocks();
-		    	plugin.nSpleefArenas.add(a);
+		    	final String[] split = data.get(i).split(":");
+		    	if (plugin.getServer().getWorld(split[7]) == null){
+		    		plugin.getServer().getScheduler().scheduleAsyncDelayedTask(plugin, new Runnable(){
+		    			public void run(){
+		    				while (true){
+		    					if (plugin.getServer().getWorld(split[7]) != null){
+		    						addArena(split);
+		    						break;
+		    					}
+		    				}
+		    			}
+		    		});
+		    	}else {
+		    		addArena(split);
+		    	}
 		    }
+	  }
+	  
+	  public static void addArena(String[] split){
+	    	nSpleefArena a = new nSpleefArena(split[0],plugin.getServer().getWorld(split[7]), plugin);
+	    	a.setFirstBlock(Util.toVectorxyz(Integer.parseInt((split[1])), Integer.parseInt((split[2])), Integer.parseInt((split[3]))));
+	    	a.setSecondBlock(Util.toVectorxyz(Integer.parseInt((split[4])), Integer.parseInt((split[5])), Integer.parseInt((split[6]))));
+	    	a.setTpBlock();
+			a.createVolume();
+			a.getVolume().setCornerOne(a.getFirstBlock().toLocation(a.getWorld()).getBlock());
+			a.getVolume().setCornerTwo(a.getSecondBlock().toLocation(a.getWorld()).getBlock());
+			a.getVolume().saveBlocks();
+	    	plugin.nSpleefArenas.add(a);
 	  }
 }
